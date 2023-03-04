805c6493f9034ce00b1dd59b37d5d799b44460f2
jtrillos99 <125828155+jtrillos99@users.noreply.github.com>
2023-03-04 15:49:19 -0500

    Respuestas_Lab1

--- preguntas.py
+++ preguntas.py
@@ -1,260 +1,280 @@
 """
 Laboratorio de Programación Básica en Python para Manejo de Datos
 -----------------------------------------------------------------------------------------
-
 Este archivo contiene las preguntas que se van a realizar en el laboratorio.
-
 No puede utilizar pandas, numpy o scipy. Se debe utilizar solo las funciones de python
 básicas.
-
 Utilice el archivo `data.csv` para resolver las preguntas.
-
-
 """
 
+import csv
 
-def pregunta_01():
-    """
-    Retorne la suma de la segunda columna.
-
-    Rta/
-    214
-
-    """
-    return
-
-
-def pregunta_02():
-    """
-    Retorne la cantidad de registros por cada letra de la primera columna como la lista
-    de tuplas (letra, cantidad), ordendas alfabéticamente.
-
-    Rta/
-    [
-        ("A", 8),
-        ("B", 7),
-        ("C", 5),
-        ("D", 6),
-        ("E", 14),
-    ]
-
-    """
-    return
+def pregunta_01(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+    num=[]
+    for i in data:
+       num.append(int(i[1]))
+    
+    return sum(num)
 
 
-def pregunta_03():
-    """
-    Retorne la suma de la columna 2 por cada letra de la primera columna como una lista
-    de tuplas (letra, suma) ordendas alfabeticamente.
+def pregunta_02(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+    List_in=[]
+    for k in data:
+        List_in.append(k[0])
+    List_sort=sorted(List_in)
+    
+    List_Fin=[("A",0)]
+    count=0
+    
+    for i in List_sort:
+        if i[0] != List_Fin[count][0]:
+            List_Fin.append((i[0],1))
+            count+=1
+        elif i[0] == List_Fin[count][0]:
+            List_Fin[count]=(i[0],List_Fin[count][1]+1)
+            count+=0
+    return List_Fin
+                
 
-    Rta/
-    [
-        ("A", 53),
-        ("B", 36),
-        ("C", 27),
-        ("D", 31),
-        ("E", 67),
-    ]
+def pregunta_03(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+    List_in=[]
+    for k in data:
+        List_in.append((k[0],int(k[1])))
+    List_sort=sorted(List_in)
 
-    """
-    return
-
-
-def pregunta_04():
-    """
-    La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la cantidad de
-    registros por cada mes, tal como se muestra a continuación.
-
-    Rta/
-    [
-        ("01", 3),
-        ("02", 4),
-        ("03", 2),
-        ("04", 4),
-        ("05", 3),
-        ("06", 3),
-        ("07", 5),
-        ("08", 6),
-        ("09", 3),
-        ("10", 2),
-        ("11", 2),
-        ("12", 3),
-    ]
-
-    """
-    return
-
-
-def pregunta_05():
-    """
-    Retorne una lista de tuplas con el valor maximo y minimo de la columna 2 por cada
-    letra de la columa 1.
-
-    Rta/
-    [
-        ("A", 9, 2),
-        ("B", 9, 1),
-        ("C", 9, 0),
-        ("D", 8, 3),
-        ("E", 9, 1),
-    ]
-
-    """
-    return
+    List_Fin=[("A",0)]
+    count=0
+    
+    for i in List_sort:
+        if i[0] != List_Fin[count][0]:
+            List_Fin.append((i[0],i[1]))
+            count+=1
+        elif i[0] == List_Fin[count][0]:
+            List_Fin[count]=(i[0],List_Fin[count][1]+i[1])
+            count+=0
+    return List_Fin
 
 
-def pregunta_06():
-    """
-    La columna 5 codifica un diccionario donde cada cadena de tres letras corresponde a
-    una clave y el valor despues del caracter `:` corresponde al valor asociado a la
-    clave. Por cada clave, obtenga el valor asociado mas pequeño y el valor asociado mas
-    grande computados sobre todo el archivo.
+def pregunta_04(vdata="data.csv"):
 
-    Rta/
-    [
-        ("aaa", 1, 9),
-        ("bbb", 1, 9),
-        ("ccc", 1, 10),
-        ("ddd", 0, 9),
-        ("eee", 1, 7),
-        ("fff", 0, 9),
-        ("ggg", 3, 10),
-        ("hhh", 0, 9),
-        ("iii", 0, 9),
-        ("jjj", 5, 17),
-    ]
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
 
-    """
-    return
+    List_in=[]
+    for i in data:
+        List_in.append(i[2].strip().split("-")[1])
 
+    List_sort=sorted(List_in)
+    List_Fin=[("01",0)]
+    count=0
 
-def pregunta_07():
-    """
-    Retorne una lista de tuplas que asocien las columnas 0 y 1. Cada tupla contiene un
-    valor posible de la columna 2 y una lista con todas las letras asociadas (columna 1)
-    a dicho valor de la columna 2.
+    for i in List_sort:
+        if i != List_Fin[count][0]:
+            List_Fin.append((i,1))
+            count+=1
+        elif i == List_Fin[count][0]:
+            List_Fin[count]=(i,List_Fin[count][1]+1)
+            count+=0
 
-    Rta/
-    [
-        (0, ["C"]),
-        (1, ["E", "B", "E"]),
-        (2, ["A", "E"]),
-        (3, ["A", "B", "D", "E", "E", "D"]),
-        (4, ["E", "B"]),
-        (5, ["B", "C", "D", "D", "E", "E", "E"]),
-        (6, ["C", "E", "A", "B"]),
-        (7, ["A", "C", "E", "D"]),
-        (8, ["E", "D", "E", "A", "B"]),
-        (9, ["A", "B", "E", "A", "A", "C"]),
-    ]
-
-    """
-    return
+    return List_Fin
 
 
-def pregunta_08():
-    """
-    Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
-    de la segunda columna; la segunda parte de la tupla es una lista con las letras
-    (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
-    valor de la segunda columna.
+def pregunta_05(vdata="data.csv"):
 
-    Rta/
-    [
-        (0, ["C"]),
-        (1, ["B", "E"]),
-        (2, ["A", "E"]),
-        (3, ["A", "B", "D", "E"]),
-        (4, ["B", "E"]),
-        (5, ["B", "C", "D", "E"]),
-        (6, ["A", "B", "C", "E"]),
-        (7, ["A", "C", "D", "E"]),
-        (8, ["A", "B", "D", "E"]),
-        (9, ["A", "B", "C", "E"]),
-    ]
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+    List_in=[]
+    for k in data:
+        List_in.append((k[0],int(k[1])))
+    List_sort=sorted(List_in)
+    
 
-    """
-    return
-
-
-def pregunta_09():
-    """
-    Retorne un diccionario que contenga la cantidad de registros en que aparece cada
-    clave de la columna 5.
-
-    Rta/
-    {
-        "aaa": 13,
-        "bbb": 16,
-        "ccc": 23,
-        "ddd": 23,
-        "eee": 15,
-        "fff": 20,
-        "ggg": 13,
-        "hhh": 16,
-        "iii": 18,
-        "jjj": 18,
-    }
-
-    """
-    return
+    List_Fin=[("A",List_sort[0][1],List_sort[0][1])]
+    count=0
+    
+    for i in List_sort:
+        if i[0] != List_Fin[count][0]:
+            List_Fin.append((i[0],i[1],i[1]))
+            count+=1
+        elif i[0] == List_Fin[count][0]:
+            if i[1]>=List_Fin[count][1]:
+                List_Fin[count]=(i[0],i[1],List_Fin[count][2])
+            elif i[1]<=List_Fin[count][2]:
+                List_Fin[count]=(i[0],List_Fin[count][1],i[1])
+            count+=0
+    return List_Fin
 
 
-def pregunta_10():
-    """
-    Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
-    cantidad de elementos de las columnas 4 y 5.
 
-    Rta/
-    [
-        ("E", 3, 5),
-        ("A", 3, 4),
-        ("B", 4, 4),
-        ...
-        ("C", 4, 3),
-        ("E", 2, 3),
-        ("E", 3, 3),
-    ]
+def pregunta_06(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
 
+    List_in=[]
+    for i in data:
+        List_in.append(i[4].split(","))
+  
+    List_Fin=[('aaa',1,1)]
+    L_t=[]
+    count=0
+    for i in List_in:
+        for k in i:
+            vt=k.split(":")
+            vt[1]=int(vt[1])
+            L_t.append(vt)
+    L_t=sorted(L_t)
 
-    """
-    return
-
-
-def pregunta_11():
-    """
-    Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
-    columna 4, ordenadas alfabeticamente.
-
-    Rta/
-    {
-        "a": 122,
-        "b": 49,
-        "c": 91,
-        "d": 73,
-        "e": 86,
-        "f": 134,
-        "g": 35,
-    }
+    for i in L_t:
+         if i[0] != List_Fin[count][0]:
+                List_Fin.append((i[0],i[1],i[1]))
+                count+=1
+         elif i[0] == List_Fin[count][0]:
+                    List_Fin[count]=(i[0],min(i[1],List_Fin[count][1]),max(i[1],List_Fin[count][2]))         
+            
+    return List_Fin
 
 
-    """
-    return
+def pregunta_07(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+
+    List_in=[(0,[])]
+           
+    L_data=[]
+    for i in data:
+        L_data.append((int(i[1]),i[0]))
+    List_in={0:[]}
+
+    for i in L_data:
+        if i[0] not in List_in:
+            List_in[i[0]]=[i[1]]
+        else:
+            List_in[i[0]].append(i[1])
+    List_in=sorted(List_in.items())        
+    
+    return List_in
+
+def pregunta_08(vdata="data.csv"):
+
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+
+    List_in=[(0,[])]
+           
+    L_data=[]
+    for i in data:
+        L_data.append((int(i[1]),i[0]))
+
+    L_data=sorted(L_data, key=lambda x: x[1])
+    
+    List_in={0:[]}
+
+    for i in L_data:
+        if i[0] not in List_in:
+            List_in[i[0]]=[i[1]]
+        else:
+            if i[1] not in List_in[i[0]]: 
+                List_in[i[0]].append(i[1])
+            elif i[1] is List_in[i[0]]:
+                pass
+    List_in=sorted(List_in.items())        
+    return List_in
 
 
-def pregunta_12():
-    """
-    Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
-    los valores de la columna 5 sobre todo el archivo.
+def pregunta_09(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
 
-    Rta/
-    {
-        'A': 177,
-        'B': 187,
-        'C': 114,
-        'D': 136,
-        'E': 324
-    }
+    List_in=[]
+    for i in data:
+        List_in.append(i[4].split(","))
+    L_t=[]
+    for i in List_in:
+        for k in i:
+            vt=k.split(":")
+            vt[1]=int(vt[1])
+            L_t.append(vt)
+    L_t=sorted(L_t)
 
-    """
-    return
+    Dic_fin={}
+
+    for i in L_t:
+        if i[0] not in Dic_fin:
+            Dic_fin[i[0]]=1
+        elif i[0] in Dic_fin:
+            Dic_fin[i[0]]+=1
+
+    return Dic_fin
+
+
+def pregunta_10(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+
+    List_in=[]
+    for i in data:
+        List_in.append((i[0],len(i[3].split(",")),len(i[4].split(","))))
+   
+    return List_in
+
+
+
+def pregunta_11(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+    
+    Dict_Fin1={}
+    for i in data:
+        for k in i[3].split(","):
+            if k not in Dict_Fin1:
+                Dict_Fin1[k]=int(i[1])
+            elif k in Dict_Fin1:
+                Dict_Fin1[k]+=int(i[1])
+    Dict_Fin={}
+    for i in sorted(Dict_Fin1.keys()):
+        Dict_Fin[i]=Dict_Fin1[i]
+    
+    return Dict_Fin
+
+def pregunta_12(vdata="data.csv"):
+    open_file = open(vdata, encoding = "utf8")
+    read_file = csv.reader(open_file, delimiter = '\t')
+    data = list(read_file)
+
+    Dict_Fin1={}
+    for i in data:
+        if i[0] not in Dict_Fin1:
+            Dict_Fin1[i[0]]=0
+        elif i[0] in Dict_Fin1:pass
+    
+    for i in data:
+            for k in i[4].split(","):
+                for l in k.split(":"):
+                    try: l=int(l)
+                    except: pass
+                    if isinstance(l , int):
+                        Dict_Fin1[i[0]]+=l
+                    elif isinstance(l , str):
+                        pass
+    Dict_Fin={}
+    for i in sorted(Dict_Fin1.keys()):
+        Dict_Fin[i]=Dict_Fin1[i]
+   
+    return Dict_Fin
